3f1172d38477c8ede81525151a6156de84924bfe
Chris Kaknevicius <chris.kaknevicius@Chriss-MacBook-Pro.local>
2018-12-21 17:21:36 -0500

    initial commit

---
+++ build/precache-manifest.2a7b3d95bc2a4e6348830386e2dc16c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "00b0da2353bc0bded089",
+    "url": "/static/css/main.1c597468.chunk.css"
+  },
+  {
+    "revision": "00b0da2353bc0bded089",
+    "url": "/static/js/main.00b0da23.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e39110ff30577bccccd599e987c1f096",
+    "url": "/index.html"
+  }
+];
